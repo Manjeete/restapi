2c8d4d7f3ae2783f757b982528ca92f0f5d15997
Manjeet <43195111+Manjeete@users.noreply.github.com>
2020-01-08 23:11:35 +0530

    add post and get request

--- app.py
+++ app.py
@@ -13,10 +13,50 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #init db
 db = SQLAlchemy(app)
-
 #init marshmallow
 ma = Marshmallow(app)
 
+#model
+class Dog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    breed = db.Column(db.String(200), unique=True)
+    url = db.Column(db.String(400))
+    averageAge = db.Column(db.Integer)
+    description = db.Column(db.String(1000))
+
+    def __init__(self,breed,url,averageAge,description):
+        self.breed = breed
+        self.url = url
+        self.averageAge = averageAge
+        self.description = description
+
+class DogSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'breed', 'url', 'averageAge', 'description')  
+#init schema
+dog_schema = DogSchema( )
+dogs_schema = DogSchema(many=True)   
+
+@app.route('/dog', methods=['POST'])
+def add_dog():
+    breed = request.json['breed']
+    url = request.json['url']
+    averageAge = request.json['averageAge']
+    description = request.json['description']
+
+    new_dog = Dog(breed, url, averageAge, description)
+
+    db.session.add(new_dog)
+    db.session.commit()
+
+    return dog_schema.jsonify(new_dog)
+
+# fetch all data
+@app.route('/dog',methods=['GET'])
+def get_dogs():
+    all_dogs = Dog.query.all()
+    result = dogs_schema.dump(all_dogs)
+    return jsonify(result)
 
 # run server
 if __name__ == '__main__':
